07dede03f6a04cd335700c18355053596bc86f77
Your Name <you@example.com>
2025-06-03 22:38:48 +0300

    ADD MODULES

---
+++ modules/comments.js
@@ -0,0 +1,16 @@
+export let comments = [
+  {
+    name: "Глеб Фокин",
+    date: `${formattedDate}`,
+    text: "Это будет первый комментарий на этой странице",
+    likes: 3,
+    isLiked: true
+  },
+  {
+    name: "Варвара Н.",
+    date: `${formattedDate}`,
+    text: "Мне нравится как оформлена эта страница! ❤",
+    likes: 75,
+    isLiked: false
+  },
+];
