18abefdbf28c193ee8c5314c5d0eaf2661856fda
Kristina-Boo <kristina8abdulina@gmail.com>
2025-06-10 23:33:20 +0300

    add modules

---
+++ modules/renderComments.js
@@ -0,0 +1,61 @@
+import { comments } from "./comments.js";
+import { initLikeListeners, initReplayListeners } from "./modules/initListeners.js";
+
+export    const renderComments = () => {
+  
+      const list = document.querySelector(".comments");
+      list.innerHTML = comments.map((comment, index) => {
+        return `
+        <li class="comment" data-index="${index}">
+           <div class="comment-header">
+              <div>${comment.name}</div>
+              <div id="date">${comment.date}</div>
+           </div>
+           <div class="comment-body">
+              <div class="comment-text">${comment.text}</div>
+           </div>
+           <div class="comment-footer">
+             <div class="likes">
+               <span class="likes-counter">${comment.likes}</span>
+               <button 
+               data-index="${index}" 
+               class="like-button ${comment.isLiked ? "-active-like" : ""}">
+               </button>
+             </div>
+           </div>
+         </li>
+         `;
+         }) 
+        .join("");
+      const likeButtons = document.querySelectorAll(".like-button");
+      for (const likeButton of likeButtons) {
+        likeButton.addEventListener("click", () => {
+          event.stopPropagation();
+          const index = likeButton.dataset.index;
+          const comment = comments[index];
+
+          comment.likes = comment.isLiked 
+          ? comment.likes - 1
+          : comment.likes + 1;
+
+          comment.isLiked = !comment.isLiked;
+          renderComments();
+
+        });
+      }
+
+      const commentElements = document.querySelectorAll(".comment");
+      for (const commentElement of commentElements) {
+        commentElement.addEventListener("click", () => {
+          const currentComment = comments[commentElement.dataset.index];
+          text.value = `${currentComment.name}:${currentComment.text}`;
+
+
+        });
+        
+      }
+      const comment = `$list.innerHTML ${formattedDate}`;
+
+            
+    };
+   
