f0941c0eaaed28efb16430f2da65e6554b89ced4
Your Name <you@example.com>
2025-07-19 00:58:57 +0300

    add 3th homework api, error processing

--- modules/api.js
+++ modules/api.js
@@ -1,10 +1,11 @@
 const host = 'https://wedev-api.sky.pro/api/v1/:kristina-boo'
 export const fetchComments = () => {
-    return fetch(host+"/comments").then(res =>{
+    return fetch(host + '/comments')
+    .then((res) => {
         return res.json()
     })
     .then((responseData) => {
-        const appComments = responseData.comments.map(comment => {
+        const appComments = responseData.comments.map((comment) => {
             return {
                 name: comment.author.name,
                 date: new Date (comment.date),
@@ -18,12 +19,26 @@ export const fetchComments = () => {
 }
 
 export const postComment = (text, name) => {
-    return fetch(host + "/comments", {
+    return fetch(host + '/comments', {
       method: "POST",
       body: JSON.stringify ({
         text,
         name,
-      })
+      }),
+    })
+    .then((response) => {
+        if (response.status === 500) {
+            throw new Error('Ошибка сервера')
+        }
+
+        if (response.status === 400) {
+            throw new Error ('Неверный запрос')
+        }
+
+        if (response.status === 201) {
+            return response.json()
+        }
+        
     })
     .then(() => {
         return fetchComments()
